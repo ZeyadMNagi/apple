9d5b32f8ac8016e8e198829997b7432d497ae3eb
ZeyadMNagi <zeyadnagi421@gmail.com>
2024-02-17 09:46:46 +0200

    first commit

---
+++ apple main.js
@@ -0,0 +1,9 @@
+ let img = document.querySelector('.img');
+ let contaner = document.querySelector('.contaner');
+ 
+function phones(phone){
+    img.src = phone;;
+}
+function colors(color){
+            contaner.style.background = color ;
+}
